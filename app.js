dbac9e5789c46c8df994edb0909076b40f5be887
Walter Clayton <walthedude@gmail.com>
2020-01-13 11:19:50 +0100

    added a node app for deployment to heroku

---
+++ app.js
@@ -0,0 +1,26 @@
+
+var express             = require("express"),
+    app                 = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index");
+})
+app.get("/home", function(req, res){
+    res.render("home");
+})
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
+// CONNECT TO PORTS
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER IS RUNNING!");
+});
+
+var port = process.env.PORT || 8000;
+app.listen(8000, function(){
+	console.log("ready on port " + port);
+});
