f694a6a1f5a826f26a2dc0f9647da440f42bad09
walter-clayton <walter.h.clayton@gmail.com>
2021-05-10 21:52:14 +0200

    removed reload module

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require("path");
 var express = require("express");
 var http = require("http");
 var app = express();
-var reload = require("reload");
 const { env } = require("process");
 var publicDirectory = path.join(__dirname, "public");
 
@@ -28,10 +27,6 @@ app.get("/about", function(req, res){
 
 var server = http.createServer(app);
 
-reload(app).then(function () {
-    server.listen(app.get("port"), function (){
-        console.log("Web server listening on port" + app.get("port"))
-    });
-}).catch(function (err) {
-    console.log("Reload could not restart, could not refresh/sample app", err)
-});
+server.listen(app.get("port"), function (){
+    console.log("Web server listening on port" + app.get("port"))
+})
