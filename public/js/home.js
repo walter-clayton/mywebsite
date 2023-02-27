a3fc891e4903b38c7653d2d283ab6cdb10f38b51
Walter Clayton <walter.h.clayton@gmail.com>
2023-02-27 08:20:41 +0100

    fixed small issues with nav and console

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,11 @@
 $(window).on("load",function() {
     function fade(pageLoad) {
       const windowTop=$(window).scrollTop(), windowBottom=windowTop+$(window).innerHeight();
-      console.log("windowTop: " + windowTop)
       const min=0.1, max=1, threshold=0.01;
       
       $(".fade").each(function() {
         /* Check the location of each desired element */
         const objectHeight=$(this).outerHeight(), objectTop=$(this).offset().top, objectBottom=$(this).offset().top+objectHeight;
-        console.log(objectHeight)
         /* Fade element in/out based on its visible percentage */
         if (objectTop < windowTop) {
           if (objectBottom > windowTop) {$(this).fadeTo(0,min+((max-min)*((objectBottom-windowTop)/objectHeight)/10));}
