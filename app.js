c863ac8c980d35049de2987f2539e1db4abded02
Walter Clayton <walter.h.clayton@gmail.com>
2024-10-14 15:32:43 +0200

    adjusted for vercel set up

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 var path = require("path");
 var express = require("express");
-var http = require("http");
 var app = express();
-const { env } = require("process");
 var publicDirectory = path.join(__dirname, "public");
 
-app.set("port", process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 app.set("view engine", "ejs");
 app.use(express.static(publicDirectory));
 
 app.locals.cardData = require('./data/cards.json');
-let cardData = app.locals.cardData
 app.locals.reviewData = require('./data/reviews.json');
-let reviewData = app.locals.reviewData
 
 app.get("/", function(req, res){
     res.render("home");
-})
+});
+
 app.get("/about", function(req, res){
     res.render("about");
-})
+});
 
+module.exports = app;
+
+var http = require("http");
 var server = http.createServer(app);
 
 server.listen(app.get("port"), function (){
